aa4be49a22f4d9a28687966483147cd68068d2ab
Igor <igor.warlog@gmail.com>
2026-04-09 23:46:57 -0300

    feat_extrair: Configuração do get_endpoint, e criação do constantes

--- extrair.py
+++ extrair.py
@@ -1,5 +1,12 @@
+from json import load
 import os
-import ipdb; ipdb.set_trace()
+import requests
+from constantes import URL_BASE
+from dotenv import load_dotenv
+import pprint
+
+load_dotenv()
+# import pdb; pdb.set_trace()
 
 def get_endpoint(endpoint, endpoint_id=None, params=None):
     
@@ -9,8 +16,16 @@ def get_endpoint(endpoint, endpoint_id=None, params=None):
         "APP_KEY": os.environ["APP_KEY"],
         "ResourceVersion": "v4"
     }
-    ipdb.set_trace()
-
+    if endpoint and endpoint_id:
+        url = f"{URL_BASE}/{endpoint}/{endpoint_id}"
+    elif endpoint and endpoint_id and params:
+        url = f"{URL_BASE}/{endpoint}/{endpoint_id}/?{params}"
+    elif endpoint:
+        url = f"{URL_BASE}/{endpoint}"
+    else:
+        raise ValueError("Endpoint is required")
+    response = requests.get(url, headers=headers, params=None)
+    return response.json()
     
 if __name__ == "__main__":
-    get_endpoint("flights")
+    pprint.pprint(get_endpoint("airlines"))
